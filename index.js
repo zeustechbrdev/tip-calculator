374611930bf60f7bdfb749e7979e6269bdf9182a
Zeus <zeustech.dev@gmail.com>
2024-10-10 08:06:09 -0300

    fix: remove logs and unecessary code

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const calculateTip = (event) => {
   const tagTipvalue = document.getElementById("tipvalue");
   const tagOriginalValue = document.getElementById("originalvalue");
   event.preventDefault();
-  console.log("TESTE");
   const formData = new FormData(event.target);
   const value = formData.get("value");
   const percentage = formData.get("percentage");
@@ -17,19 +16,4 @@ const calculateTip = (event) => {
   tagOriginalValue.textContent = value + "R$";
   tagTipvalue.textContent = percentageValue + "R$";
   tagTotalValue.textContent = `${Number(value) + percentageValue}R$`;
-
-  console.log(value, percentage);
 };
-
-/*
-        <h1 id="totalvalue" class="align-center mt-2">200R$</h1>
-        <div class="flex gap-2 mt-4">
-          <div>
-            <p>Valor da gorjeta</p>
-            <p id="tipvalue" class="mt-1">100 R$</p>
-          </div>
-          <div>
-            <p>Valor original</p>
-            <p id="originalvalue" class="mt-1">100 R$</p>
-          </div>
-*/
